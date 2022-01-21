30cdd0623928429abf43c3699a00d7a2cfeecbff
Denis <Denis@LAPTOP-M3KL7228>
2022-01-21 16:55:22 +0300

    Добавьте файлы проекта.

---
+++ Raduga/Form1.Designer.cs
@@ -0,0 +1,91 @@
+﻿
+namespace Raduga
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.pictureBox2 = new System.Windows.Forms.PictureBox();
+            this.pictureBox3 = new System.Windows.Forms.PictureBox();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Location = new System.Drawing.Point(712, 371);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(71, 15);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox1_Paint);
+            // 
+            // pictureBox2
+            // 
+            this.pictureBox2.Location = new System.Drawing.Point(13, 230);
+            this.pictureBox2.Name = "pictureBox2";
+            this.pictureBox2.Size = new System.Drawing.Size(306, 156);
+            this.pictureBox2.TabIndex = 1;
+            this.pictureBox2.TabStop = false;
+            this.pictureBox2.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox2_Paint);
+            // 
+            // pictureBox3
+            // 
+            this.pictureBox3.Location = new System.Drawing.Point(13, 150);
+            this.pictureBox3.Name = "pictureBox3";
+            this.pictureBox3.Size = new System.Drawing.Size(306, 74);
+            this.pictureBox3.TabIndex = 2;
+            this.pictureBox3.TabStop = false;
+            this.pictureBox3.Paint += new System.Windows.Forms.PaintEventHandler(this.pictureBox3_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(795, 394);
+            this.Controls.Add(this.pictureBox3);
+            this.Controls.Add(this.pictureBox2);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.PictureBox pictureBox2;
+        private System.Windows.Forms.PictureBox pictureBox3;
+    }
+}
+
---
+++ Raduga/Form1.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Raduga
+{
+    public partial class Form1 : Form
+    {
+        Point center;
+        PaintEventArgs pai;
+        Bitmap MyImage;
+public Form1()
+        {
+            InitializeComponent();
+
+            this.Paint += pictureBox1_Paint;
+            this.Paint += pictureBox2_Paint;
+            this.Paint += pictureBox2_Paint;
+        }
+
+        private void pictureBox1_Paint(object sender, PaintEventArgs e)
+        {
+            for (int i = 0; i < 10; i++)
+            {
+                DrawPolygon(e.Graphics, 5, new Point(90 + (i * 120), 50), 30);
+                DrawCurve(e.Graphics, 10, new Point(30 + (i * 120), 50), 30);
+            }
+            
+        }
+
+        private void DrawPolygon(Graphics g, int count, Point center, float r)
+        {
+            // угол поворота (чтобы рисовал, начиная от оси oy)
+            double angle = -Math.PI * 0.5;
+
+            // создаем точки
+            Point[] points = new Point[count];
+
+            for (int i = 0; i < count; i++)
+            { 
+                points[i] = new Point(
+                    center.X + (int)Math.Round(Math.Cos(angle + Math.PI * 2.0 * i / count) * r),
+                    center.Y + (int)Math.Round(Math.Sin(angle + Math.PI * 2.0 * i / count) * r)
+                );
+            }
+
+            SolidBrush br = new SolidBrush(Color.Blue);
+
+            // рисуем многоугольник
+            g.FillPolygon(br, points);
+        }
+
+        private void DrawCurve(Graphics g, int count, Point center, float r)
+        {
+            // угол поворота (чтобы рисовал, начиная от оси oy)
+            double angle = -Math.PI * 0.5;
+
+            // создаем точки
+            Point[] points = new Point[count];
+
+            for (int i = 0; i < count; i++)
+            {
+                points[i] = new Point(
+                    center.X + (int)Math.Round(Math.Cos(angle + Math.PI * 2.0 * i / count) * r),
+                    center.Y + (int)Math.Round(Math.Sin(angle + Math.PI * 2.0 * i / count) * r)
+                );
+            }
+
+            SolidBrush br = new SolidBrush(Color.Red);
+
+            // рисуем многоугольник
+            g.FillClosedCurve(br, points);
+        }
+
+        private void DrawRect(SolidBrush br, int x = 10, int y = 10)
+        {
+            Bitmap bmp;
+            bmp = new Bitmap(pictureBox3.Width, pictureBox3.Height);
+            pictureBox3.Image = bmp;
+            Graphics g = Graphics.FromImage(pictureBox3.Image);
+            Rectangle rect = new Rectangle(x, y, 500, 10);
+            br = new SolidBrush(Color.Red);
+            g.FillRectangle(br, rect);
+        }
+
+        private void pictureBox2_Paint(object sender, PaintEventArgs e)
+        {
+            pictureBox2.SizeMode = PictureBoxSizeMode.StretchImage;
+            MyImage = new Bitmap("C:/Users/Denis/source/repos/Raduga/Raduga/Resources/IMG_8639.jpg");
+            pictureBox2.ClientSize = new Size(306, 156);
+            pictureBox2.Image = (Image)MyImage;
+        }
+
+        private void pictureBox3_Paint(object sender, PaintEventArgs e)
+        {
+            Bitmap bmp;
+            bmp = new Bitmap(pictureBox3.Width, pictureBox3.Height);
+            pictureBox3.Image = bmp;
+            Graphics g = Graphics.FromImage(pictureBox3.Image);
+            Rectangle rect = new Rectangle(0, 10, 500, 10);
+            SolidBrush br = new SolidBrush(Color.Red);
+
+            g.FillRectangle(br, rect);
+
+            br = new SolidBrush(Color.Orange);
+            rect = new Rectangle(0, 20, 500, 10);
+            g.FillRectangle(br, rect);
+
+            br = new SolidBrush(Color.Yellow);
+            rect = new Rectangle(0, 30, 500, 10);
+            g.FillRectangle(br, rect);
+
+            br = new SolidBrush(Color.Green);
+            rect = new Rectangle(0, 40, 500, 10);
+            g.FillRectangle(br, rect);
+
+            br = new SolidBrush(Color.Blue);
+            rect = new Rectangle(0, 50, 500, 10);
+            g.FillRectangle(br, rect);
+
+
+            br = new SolidBrush(Color.Purple);
+            rect = new Rectangle(0, 60, 500, 10);
+            g.FillRectangle(br, rect);
+        }
+    }
+}
